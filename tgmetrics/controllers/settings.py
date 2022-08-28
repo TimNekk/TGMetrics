f747f0290366293fb564687b24c4d6a865f32179
Tim <herew26@gmail.com>
2022-08-29 02:11:54 +0300

    feat: add settings controller

---
+++ tgmetrics/controllers/settings.py
@@ -0,0 +1,28 @@
+from dash import Output, Input, Dash
+
+from tgmetrics import repositories, views
+
+
+class SettingsController:
+    def __init__(self,
+                 graph_repository: repositories.GraphRepository,
+                 settings_card_view: views.cards.SettingsCardView) -> None:
+        self._graph_repository = graph_repository
+        self._settings_card_view = settings_card_view
+
+    def register_callbacks(self, app: Dash) -> None:
+        app.callback(
+            Output(self._settings_card_view.category_dropdown_id, "options"),
+            Input(self._settings_card_view.category_dropdown_id, "value"),
+        )(self.update_category_dropdown_options)
+
+        app.callback(
+            Output(self._settings_card_view.subcategory_dropdown_id, "options"),
+            Input(self._settings_card_view.category_dropdown_id, "value"),
+        )(self.update_subcategory_dropdown_options)
+
+    def update_category_dropdown_options(self, value):
+        return self._graph_repository.categories
+
+    def update_subcategory_dropdown_options(self, value):
+        return self._graph_repository.get_subcategories(value)
